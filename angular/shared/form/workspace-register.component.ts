cb6266f4d609a0f1fd48d1d6839f40063c270b16
Moises Sacal <moisbo@gmail.com>
2020-06-26 17:00:21 +1000

    modified workspace into columns

--- angular/shared/form/workspace-register.component.ts
+++ angular/shared/form/workspace-register.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { SimpleComponent } from './field-simple.component';
 import { FieldBase } from './field-base';
 import { WorkspaceRegisterField } from './workspace-field.component';
 import * as _ from "lodash";
 declare var jQuery: any;
 import { CompleterService, CompleterData } from 'ng2-completer';
-
 
 @Component({
   selector: 'workspace-register-parent',
@@ -36,14 +36,16 @@ export class WorkspaceRegisterFieldComponent extends WorkspaceRegisterComponent 
   field: WorkspaceRegisterField
   static clName = 'WorkspaceRegisterFieldComponent';
   disabledType: boolean = false;
-  stepHidden = [false,true,true,true,true];
+  stepHidden = [false,true];
   vstate = 0;
   searchStr: string;
   dataService: CompleterData;
   searchData = [];
   workspaceSelected: any = {};
-  workspaceRegister: WorkspaceRegister;
+  workspaceType: WorkspaceType;
   clearSelected: boolean = false;
+  workspaceRegisterForm;
+  submitted = false;
 
   ngOnInit() {
     this.field.init();
@@ -54,78 +56,64 @@ export class WorkspaceRegisterFieldComponent extends WorkspaceRegisterComponent 
     this.fieldMap._rootComp.onSubmit().subscribe(response => {
       this.disabledType = false;
       this.vstate = 0;
-      this.stepHidden = [false,true,true,true,true];
-      this.workspaceRegister = new WorkspaceRegister();
+      this.stepHidden = [false,true];
+      this.workspaceRegisterForm.reset();
       this.clearSelected = true;
-      jQuery('#workspaceRegisterModal').modal('show');
+      this.submitted = false;
+      jQuery('#workspaceRegisterModal').modal({backdrop: 'static', keyboard: false, show: true});
     });
   }
 
   onSelect(event) {
     this.disabledType = true;
-    this.workspaceRegister.workspaceType = event['originalObject']
+    this.workspaceType = event['originalObject'];
+    this.stepHidden = [true,false];
   }
 
   clearWorkspaceSelected() {
     this.clearSelected = true;
     this.disabledType = false;
+    this.stepHidden = [false,true];
   }
 
-  step(w) {
-    if(w === 'next') {
-      if(this.vstate > 3){
-        this.vstate = 4;
-      } else {
-        this.vstate++;
-      }
-    } else {
-      if(this.vstate < 1) {
-        this.vstate = 0;
-      } else {
-        this.vstate--;
-      }
+  registerWorkspace(value) {
+    console.log(`registerWorkspace ${value}`);
+    this.workspaceRegisterForm.patchValue({workspaceType: this.workspaceType.value});
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.workspaceRegisterForm.invalid) {
+      return;
     }
-    _.each(this.stepHidden, (t, i) => {
-      this.stepHidden[i] = this.vstate != i;
-    });
   }
 
-  registerWorkspace() {
-    console.log('registerWorkspace');
-    console.log(this.workspaceRegister);
-  }
+  get f() { return this.workspaceRegisterForm.controls; }
 
-  constructor(private completerService: CompleterService) {
+  constructor(private completerService: CompleterService,
+              private formBuilder: FormBuilder) {
     super();
-    console.log(this.field);
     this.searchData = [
       {label: "eResearch Store", value: "research-workspace-eresearch-store", description: 'Storage provided by eResearch', image: 'https://stash-uat.research.uts.edu.au/angular/catalog/assets/images/storage.png'},
-      {label: "OneDrive", value: "research-workspace-onedrive", keywords:'microsoft storage', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Microsoft_Office_OneDrive_%282018%E2%80%93present%29.svg'},
-      {label: "Cloudstor", value: "research-workspace-cloudstor", keywords: 'cloudstor storage',image: '/angular/assets/images/cloudstor.png'},
-      {label: "University Drive", value: "research-workspace-university-drive", keywords:'uts storage', image: 'https://www.lib.uts.edu.au/sites/all/themes/utslib2011/images/logos/UTS_logo_web.svg'},
-      {label: "Qualtrics", value: "research-workspace-eresearch-qualtrics", keywords:'survey', image: 'https://www.qualtrics.com/m/qualtrics-xm-long.svg'},
-      {label: "eResearch LimeSurvey", value: "research-workspace-eresearch-limesurvey", keywords:'survey', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Limesurvey_logo.png'},
-      {label: "HPCC", value: "research-workspace-eresearch-hpcc", keywords:'High Performance Computer Cluster', image: 'https://stash-uat.research.uts.edu.au/angular/catalog/assets/images/uts_hpcc.png'},
-      {label: "iHPCC", value: "research-workspace-eresearch-ihpc", keywords: 'Interactive High Performance Computer', image: 'https://stash-uat.research.uts.edu.au/angular/catalog/assets/images/uts_ihpc.png'},
-      {label: "Git Repository", value: "research-workspace-eresearch-git-repository", keywords: 'github gitlab', image: 'https://git-scm.com/images/logo@2x.png'}
+      {label: "OneDrive", value: "research-workspace-onedrive", keywords:'microsoft storage', description: 'Store and share photos, videos, documents', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Microsoft_Office_OneDrive_%282018%E2%80%93present%29.svg'},
+      {label: "Cloudstor", value: "research-workspace-cloudstor", keywords: 'cloudstor storage', description: 'Cloud service for researchers. With CloudStor researchers can easily sync, share and store files using the high-speed AARNet network', image: '/angular/assets/images/cloudstor.png'},
+      {label: "University Drive", value: "research-workspace-university-drive", keywords:'uts storage', description: 'G:/ or H:// drive provided by UTS', image: 'https://www.lib.uts.edu.au/sites/all/themes/utslib2011/images/logos/UTS_logo_web.svg'},
+      {label: "Qualtrics", value: "research-workspace-eresearch-qualtrics", keywords:'survey', description: 'Build surveys, distribute surveys and analyze responses ', image: 'https://www.qualtrics.com/m/qualtrics-xm-long.svg'},
+      {label: "eResearch LimeSurvey", value: "research-workspace-eresearch-limesurvey", keywords:'survey', description: 'Online survey tool that allows one to build surveys, distribute surveys and analyze responses ', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d4/Limesurvey_logo.png'},
+      {label: "HPCC", value: "research-workspace-eresearch-hpcc", keywords:'High Performance Computer Cluster', description: 'High Performance Computing Cluster (HPCC) that can be accessed by UTS researchers.', image: 'https://stash-uat.research.uts.edu.au/angular/catalog/assets/images/uts_hpcc.png'},
+      {label: "iHPCC", value: "research-workspace-eresearch-ihpc", keywords: 'Interactive High Performance Computer', description: 'The iHPC facility provides an interactive high performance computing resource for all researchers within UTS.', image: 'https://stash-uat.research.uts.edu.au/angular/catalog/assets/images/uts_ihpc.png'},
+      {label: "Git Repository", value: "research-workspace-eresearch-git-repository", keywords: 'github gitlab', description: 'Repositories in GIT contain a collection of files of various different versions of a Project', image: 'https://git-scm.com/images/logo@2x.png'}
     ];
-    this.workspaceRegister = {
-      name: '',
-      workspaceType: new WorkspaceType(),
-      location: '',
-      details: ''
-    }
     this.dataService = completerService.local(this.searchData, 'label,description,keywords', 'label').imageField("image").descriptionField("description");
+    this.workspaceRegisterForm = this.formBuilder.group({
+      name: new FormControl('', Validators.required),
+      location: new FormControl('', Validators.required),
+      details: new FormControl('', Validators.required),
+      workspaceType: new FormControl('', Validators.required),
+    });
  }
 
 }
 
-class WorkspaceRegister {
-  workspaceType: WorkspaceType = new WorkspaceType();
-  name: string = '';
-  location: string = '';
-  details: string = '';
-}
 class WorkspaceType{
   label: string = '';
   description: string = '';
