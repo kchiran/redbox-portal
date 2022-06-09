2696bc9f6901aad9eb0dc1a9e531e0d4c5ccf8fa
root <root@er-stash-v3-dev.itd.uts.edu.au>
2022-06-09 14:15:31 +1000

    Resolve dependency issues

--- config/recordtype.js
+++ config/recordtype.js
@@ -17,21 +17,18 @@ module.exports.recordtype = {
               "metadata.contributor_data_manager",
               "metadata.contributor_supervisor",
               "metadata.contributors"
-            ],
-            "recordCreatorPermissions" : "view&edit"
+            ]
           }
         }],
         // Requires the PDF Gen hook to be installed https://www.npmjs.com/package/@researchdatabox/sails-hook-redbox-pdfgen
-        // post: [{
-        //
-        //   function: 'sails.services.pdfservice.createPDF',
-        //   options: {
-        //     waitForSelector: 'div#loading.hidden',
-        //     pdfPrefix: 'rdmp-pdf',
-        //     // Need to set an API token for generation to occur
-        //     // token: 'abcd-efgh-abcd-abcd-abcd'
-        //   }
-        // }]
+        post: [{
+          function: 'sails.services.pdfservice.createPDF',
+          options: {
+            waitForSelector: 'div#loading.hidden',
+            pdfPrefix: 'rdmp-pdf',
+            token: '{{ REDBOX_PORTAL_API_KEY }}'
+          }
+        }]
       },
       onUpdate: {
         pre: [{
@@ -48,18 +45,18 @@ module.exports.recordtype = {
               "metadata.contributor_data_manager",
               "metadata.contributor_supervisor",
               "metadata.contributors"
-            ],
-            "recordCreatorPermissions" : "view&edit"
+            ]
           }
         }],
         // Requires the PDF Gen hook to be installed https://www.npmjs.com/package/@researchdatabox/sails-hook-redbox-pdfgen
-        // post: [{
-        //   function: 'sails.services.pdfservice.createPDF',
-        //   options: {
-        //     waitForSelector: 'div#loading.hidden',
-        //     pdfPrefix: 'rdmp-pdf'
-        //   }
-        // }]
+        post: [{
+          function: 'sails.services.pdfservice.createPDF',
+          options: {
+            waitForSelector: 'div#loading.hidden',
+            pdfPrefix: 'rdmp-pdf',
+            token: '{{ REDBOX_PORTAL_API_KEY }}'
+          }
+        }]
       }
     },
     relatedTo: [{
@@ -67,9 +64,7 @@ module.exports.recordtype = {
       "foreignField": "metadata.rdmp.oid"
     }],
     transferResponsibility: {
-      /*
-        Defines the fields that map to roles in the record
-      */
+      // Defines the fields that map to roles in the record
       fields: {
         chiefInvestigator: {
           label: "@dmpt-people-tab-ci", // The label to show in the radio button options
@@ -78,7 +73,10 @@ module.exports.recordtype = {
         },
         dataManager: {
           label: "@dmpt-people-tab-data-manager", // The label to show in the radio button options
-          updateField: 'contributor_data_manager'
+          updateField: 'contributor_data_manager',
+          fieldNames: {
+            text_full_name: "dataLicensingAccess_manager" // The name field in the form
+          }
         },
         dataOwner: {
           label: "@dmpt-people-tab-data-owner", // The label to show in the radio button options
@@ -89,8 +87,8 @@ module.exports.recordtype = {
         }
       },
       /*
-        canEdit block defines which fields the user may edit if
-        they have been set as that role in the record
+       canEdit block defines which fields the user may edit if
+       they have been set as that role in the record
       */
       canEdit: {
         dataManager: ["dataManager", "chiefInvestigator", "dataOwner"],
@@ -99,11 +97,11 @@ module.exports.recordtype = {
       }
     },
     searchFilters: [{
-        name: "text_title",
-        title: "search-refine-title",
-        type: "exact",
-        typeLabel: "search-refine-contains"
-      },
+      name: "text_title",
+      title: "search-refine-title",
+      type: "exact",
+      typeLabel: "search-refine-contains"
+    },
       {
         name: "text_description",
         title: "search-refine-description",
@@ -140,11 +138,11 @@ module.exports.recordtype = {
       namePlural: "Records"
     },
     searchFilters: [{
-        name: "text_title",
-        title: "search-refine-title",
-        type: "exact",
-        typeLabel: "search-refine-contains"
-      },
+      name: "text_title",
+      title: "search-refine-title",
+      type: "exact",
+      typeLabel: "search-refine-contains"
+    },
       {
         name: "text_description",
         title: "search-refine-description",
@@ -178,10 +176,10 @@ module.exports.recordtype = {
       "localField": "metadata.rdmp.oid",
       "foreignField": "redboxOid"
     },
-    {
-      "recordType": "dataPublication",
-      "foreignField": "metadata.dataRecord.oid"
-    }],
+      {
+        "recordType": "dataPublication",
+        "foreignField": "metadata.dataRecord.oid"
+      }],
     transferResponsibility: {
       /*
         Defines the fields that map to roles in the record
@@ -205,8 +203,8 @@ module.exports.recordtype = {
         }
       },
       /*
-        canEdit block defines which fields the user may edit if
-        they have been set as that role in the record
+       canEdit block defines which fields the user may edit if
+       they have been set as that role in the record
       */
       canEdit: {
         dataManager: ["dataManager", "chiefInvestigator", "dataOwner"],
@@ -230,8 +228,7 @@ module.exports.recordtype = {
               "metadata.contributor_data_manager",
               "metadata.contributor_supervisor",
               "metadata.contributors"
-            ],
-            "recordCreatorPermissions" : "view&edit"
+            ]
           }
         }]
       },
@@ -250,8 +247,7 @@ module.exports.recordtype = {
               "metadata.contributor_data_manager",
               "metadata.contributor_supervisor",
               "metadata.contributors"
-            ],
-            "recordCreatorPermissions" : "view&edit"
+            ]
           }
         }]
       }
@@ -264,11 +260,11 @@ module.exports.recordtype = {
       namePlural: "Data Publications"
     },
     searchFilters: [{
-        name: "text_title",
-        title: "search-refine-title",
-        type: "exact",
-        typeLabel: "search-refine-contains"
-      },
+      name: "text_title",
+      title: "search-refine-title",
+      type: "exact",
+      typeLabel: "search-refine-contains"
+    },
       {
         name: "text_description",
         title: "search-refine-description",
@@ -300,25 +296,6 @@ module.exports.recordtype = {
     hooks: {
       onCreate: {
         pre: [
-          //Transition workflow from queued to reviewing. TODO: Condition needs to be changed to check when staging location set
-          {
-            function: 'sails.services.triggerservice.transitionWorkflow',
-            options: {
-              "triggerCondition": "<%= _.isEqual(workflow.stage, 'queued') && _.isEqual(metadata.embargoByDate, '')  %>",
-              "targetWorkflowStageName": "reviewing",
-              "targetWorkflowStageLabel": "Reviewing",
-              "targetForm": "dataPublication-1.0-reviewing"
-            }
-          },
-          {
-            function: 'sails.services.triggerservice.transitionWorkflow',
-            options: {
-              "triggerCondition": "<%= _.isEqual(workflow.stage, 'queued') && _.isEqual(metadata.embargoByDate, true) %>",
-              "targetWorkflowStageName": "embargoed",
-              "targetWorkflowStageLabel": "Embargoed",
-              "targetForm": "dataPublication-1.0-embargoed"
-            }
-          },
           // Set the notification state for draft publications
           {
             function: 'sails.services.recordsservice.updateNotificationLog',
@@ -333,12 +310,23 @@ module.exports.recordtype = {
           }
         ],
         post: [
+          // push the dataset to the staging web server
+          // this needs to be defined here for the case when the user goes straight
+          // to reviewing without saving first
+          {
+            function: 'sails.services.publicationservice.exportDataset',
+            options: {
+              triggerCondition: "<%= record.workflow.stage=='reviewing' %>",
+              site: 'staged'
+            }
+          },
+
           // `Email "data publication is staged" notification to FNCI, DM, Supervisor with link to landing page on Staging`
           {
             function: 'sails.services.emailservice.sendRecordNotification',
             options: {
               triggerCondition: "<%= record.notification != null && record.notification.state == 'draft' && record.workflow.stage == 'reviewing' %>",
-              to: "<%= record.metadata.contributor_ci.email %>,<%= record.metadata.contributor_data_manager.email %>,<%= record.metadata.contributor_supervisor.email %>",
+              to: "<%= record.metadata.contributor_ci.email %>,<%= record.metadata.contributor_data_manager.email %>,{{ DATA_LIBRARIAN_EMAIL }}",
               subject: "A publication has been staged for publishing.",
               template: "publicationStaged",
               onNotifySuccess: [
@@ -347,7 +335,7 @@ module.exports.recordtype = {
                   function: 'sails.services.emailservice.sendRecordNotification',
                   options: {
                     forceRun: true,
-                    to: "librarian@redboxresearchdata.com.au",
+                    to: "{{ DATA_LIBRARIAN_EMAIL }}",
                     subject: "Data publication ready for review",
                     template: "publicationReview"
                   }
@@ -371,7 +359,7 @@ module.exports.recordtype = {
             function: 'sails.services.emailservice.sendRecordNotification',
             options: {
               triggerCondition: "<%= record.notification != null && record.notification.state == 'emailed-reviewing' && record.workflow.stage == 'published' %>",
-              to: "<%= record.metadata.contributor_ci.email %>,<%= record.metadata.contributor_data_manager.email %>,<%= record.metadata.contributor_supervisor.email %>,librarian@redboxresearchdata.com.au,<%= _.isEmpty(record.metadata.creators) ? '' : _.join(_.map(record.metadata.creators, (creator)=>{ return creator.email; }), ',') %>",
+              to: "<%= record.metadata.contributor_ci.email %>,<%= record.metadata.contributor_data_manager.email %>,{{ DATA_LIBRARIAN_EMAIL }},<%= _.isEmpty(record.metadata.creators) ? '' : _.join(_.map(record.metadata.creators, (creator)=>{ return creator.email; }), ',') %>",
               subject: "A publication has been successfully published",
               template: "publicationPublished",
               onNotifySuccess: [
@@ -398,16 +386,7 @@ module.exports.recordtype = {
           {
             function: 'sails.services.triggerservice.transitionWorkflow',
             options: {
-              "triggerCondition": "<%= _.isEqual(workflow.stage, 'queued') && _.isEqual(metadata.embargoByDate, '') %>",
-              "targetWorkflowStageName": "reviewing",
-              "targetWorkflowStageLabel": "Reviewing",
-              "targetForm": "dataPublication-1.0-reviewing"
-            }
-          },
-          {
-            function: 'sails.services.triggerservice.transitionWorkflow',
-            options: {
-              "triggerCondition": "<%= _.isEqual(workflow.stage, 'queued') && _.isEqual(metadata.embargoByDate, true) %>",
+              "triggerCondition": "<%= _.isEqual(workflow.stage, 'draft') && _.isEqual(metadata.embargoByDate, true) %>",
               "targetWorkflowStageName": "embargoed",
               "targetWorkflowStageLabel": "Embargoed",
               "targetForm": "dataPublication-1.0-embargoed"
@@ -434,33 +413,46 @@ module.exports.recordtype = {
               flagVal: 'draft', // hard coded value
               saveRecord: false // when true, do metadata update -> false, since this is on a pre-save hook, gets saved anyway
             }
+          },
+          // Set the notification state for draft publications
+          {
+            function: 'sails.services.recordsservice.updateNotificationLog',
+            options: {
+              name: "Set Notification to Draft",
+              // when notification is undefined, start with 'draft', so skipping stages will still work (as with the shipped behavior above)
+              triggerCondition: "<%= record.workflow.stage == 'draft' && record.notification.state == 'emailed-reviewing'%>",
+              flagName: 'notification.state', // the record's path to the notification flag
+              flagVal: 'draft', // hard coded value
+              saveRecord: false // when true, do metadata update -> false, since this is on a pre-save hook, gets saved anyway
+            }
           }
         ],
         post: [
+          // push the dataset to the staging web server
           {
             function: 'sails.services.publicationservice.exportDataset',
             options: {
               triggerCondition: "<%= record.workflow.stage=='reviewing' %>",
-              site: 'staging'
+              site: '{{ REDBOX_PORTAL_DATAPUBS_STAGED_NGINX }}'
             }
           },
           {
             function: 'sails.services.emailservice.sendRecordNotification',
             options: {
-              triggerCondition: "<%= record.notification != null && record.notification.state == 'draft' && record.workflow.stage == 'reviewing' %>",
-              to: "<%= record.metadata.contributor_ci.email %>,<%= record.metadata.contributor_data_manager.email %>,<%= record.metadata.contributor_supervisor.email %>",
+              triggerCondition: "<%= record.notification.state == 'draft' && record.workflow.stage == 'reviewing'  %>",
+              to: "<%= record.metadata.contributor_ci.email %>,<%= record.metadata.contributor_data_manager.email %>,{{ DATA_LIBRARIAN_EMAIL }}",
               subject: "A publication has been staged for review.",
               template: "publicationStaged",
               onNotifySuccess: [
-          // `Email "data publication is ready for review" notification to Librarian data-librarian@uts.edu.au with a link to the data publication record`
+                // `Email "data publication is ready for review" notification to Librarian {{ DATA_LIBRARIAN_EMAIL }} with a link to the data publication record`
                 {
                   function: 'sails.services.emailservice.sendRecordNotification',
-                    options: {
-                      forceRun: true,
-                      to: "librarian@redboxresearchdata.com.au",
-                      subject: "Data publication ready for review",
-                      template: "publicationReview"
-                    }
+                  options: {
+                    forceRun: true,
+                    to: "{{ DATA_LIBRARIAN_EMAIL }}",
+                    subject: "Data publication ready for review",
+                    template: "publicationReview"
+                  }
                 },
                 {
                   function: 'sails.services.recordsservice.updateNotificationLog',
@@ -476,25 +468,50 @@ module.exports.recordtype = {
               ]
             }
           },
-
           {
             function: 'sails.services.publicationservice.exportDataset',
             options: {
               triggerCondition: "<%= record.workflow.stage=='published' %>",
-              site: 'public'
+              site: '{{ REDBOX_PORTAL_DATAPUBS_PUBLIC_NGINX }}'
             }
           },
-
-
           // Triggers "Published" Email Notification to FNCI, DM, Collaborators, CC: librarian with RDA link
           {
             function: 'sails.services.emailservice.sendRecordNotification',
             options: {
-              triggerCondition: "<%= record.notification != null && record.notification.state == 'emailed-reviewing' && record.workflow.stage == 'published' %>",
-              to: "<%= record.metadata.contributor_ci.email %>,<%= record.metadata.contributor_data_manager.email %>,<%= record.metadata.contributor_supervisor.email %>,librarian@redboxresearchdata.com.au,<%= _.isEmpty(record.metadata.creators) ? '' : _.join(_.map(record.metadata.creators, (creator)=>{ return creator.email; }), ',') %>",
+              triggerCondition: "<%= record.notification.state == 'emailed-reviewing' && record.workflow.stage == 'published' %>",
+              to: "<%= record.metadata.contributor_ci.email %>,<%= record.metadata.contributor_data_manager.email %>,{{ DATA_LIBRARIAN_EMAIL }}",
               subject: "A publication has been successfully published",
               template: "publicationPublished",
               onNotifySuccess: [
+                // `Email "data publication is ready for review" notification to Librarian {{ DATA_LIBRARIAN_EMAIL }} with a link to the data publication record`
+                {
+                  function: 'sails.services.emailservice.sendRecordNotification',
+                  options: {
+                    forceRun: true,
+                    to: "{{ DATA_LIBRARIAN_EMAIL }}",
+                    subject: "A publication has been successfully published",
+                    template: "publicationPublished"
+                  }
+                },
+                {
+                  function: 'sails.services.emailservice.sendRecordNotification',
+                  options: {
+                    forceRun: true,
+                    to: "<%= record.metadata.contributor_ci.email %>",
+                    subject: "A publication has been successfully published",
+                    template: "publicationPublished"
+                  }
+                },
+                {
+                  function: 'sails.services.emailservice.sendRecordNotification',
+                  options: {
+                    forceRun: true,
+                    to: "<%= record.metadata.contributor_data_manager.email %>",
+                    subject: "A publication has been successfully published",
+                    template: "publicationPublished"
+                  }
+                },
                 {
                   function: 'sails.services.recordsservice.updateNotificationLog',
                   options: {
@@ -512,43 +529,5 @@ module.exports.recordtype = {
         ]
       }
     }
-  },
-  "genericWorkspace": {
-    "packageType": "workspace",
-    searchFilters: [
-      {
-        name: "text_title",
-        title: "search-refine-title",
-        type: "exact",
-        typeLabel: "search-refine-contains"
-      },
-      {
-        name: "text_description",
-        title: "search-refine-description",
-        type: "exact",
-        typeLabel: "search-refine-contains"
-      },
-      {
-        name: "grant_number_name",
-        title: "search-refine-grant_number_name",
-        type: "facet",
-        typeLabel: null,
-        alwaysActive: true
-      },
-      {
-        name: "finalKeywords",
-        title: "search-refine-keywords",
-        type: "facet",
-        typeLabel: null,
-        alwaysActive: true
-      },
-      {
-        name: "workflow_stageLabel",
-        title: "search-refine-workflow_stageLabel",
-        type: "facet",
-        typeLabel: null,
-        alwaysActive: true
-      }
-    ]
   }
 };
